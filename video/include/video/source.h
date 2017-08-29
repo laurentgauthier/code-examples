b7de9e547ec032a7a450433a5db0efedc63ed9d6
Laurent Gauthier <laurent.gauthier@soccasys.com>
2017-08-30 00:38:26 +0200

    License cleanup
    Signed-off-by: Laurent Gauthier <laurent.gauthier@soccasys.com>

--- video/include/video/source.h
+++ video/include/video/source.h
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2017 Laurent GAUTHIER
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef _VIDEO_SOURCE_H_
 #define _VIDEO_SOURCE_H_
 
